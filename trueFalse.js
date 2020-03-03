928f37af9736a62bba2606df8bb46f7022fa8124
mahealomwebdev1 <mahealom.web.dev@gmail.com>
2020-03-04 00:04:17 +0600

    my files

---
+++ trueFalse.js
@@ -0,0 +1,20 @@
+ // falsy:
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+// truthy
+// "0", " ", [], "false",
+
+
+let name="false";
+console.log(name);
+if(name){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
